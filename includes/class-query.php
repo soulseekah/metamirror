9a5cee2654f663ff531bf0716c0f9ee6998040f7
Gennady Kovshenin <gennady@kovshenin.com>
2018-03-16 14:25:33 +0500

    Joins

--- includes/class-query.php
+++ includes/class-query.php
@@ -8,12 +8,21 @@ class Query {
 	private $mirrors = [];
 
 	/**
+	 * @var string[] Meta tables being mirrored by this query.
+	 */
+	private $tables = [];
+
+	/**
 	 * Mirror mirror on the wall...
 	 *
 	 * @param metamirror\Mirror[] $mirrors An array of mirrors to route to.
 	 */
 	public function __construct( array $mirrors ) {
 		$this->mirrors = $mirrors;
+
+		$this->tables = array_unique( array_map( function( $mirror ) {
+			return $mirror->meta_table;
+		}, $this->mirrors ) );
 	}
 
 	/**
@@ -28,6 +37,16 @@ class Query {
 	 * @return string The rewritten SQL.
 	 */
 	public function rewrite( string $query ) : string {
+		static $skip_pattern;
+		if ( ! $skip_pattern ) {
+			$skip_pattern = '#' . implode( '|', array_map( 'preg_quote', $this->tables ) ) . '#';
+		}
+
+		/** Skip queries that aren't even closely meta. */
+		if ( ! preg_match( $skip_pattern, $query ) ) {
+			return $query;
+		}
+
 		/** Parse the query and analyze it. */
 		$_query = self::parse( $query );
 
@@ -46,20 +65,34 @@ class Query {
 	 * Hopefully the subset we understand and the inferences we
 	 * make will work for most cases.
 	 *
+	 * This is where most issues will arise. While the core WP_*Query
+	 * classes don't do anything too fancy, custom SQL can freak the
+	 * whole method out with exotic CROSS JOIN syntaxing, etc.
+	 *
+	 * Yet, our goal is to only works when we're 100% sure that
+	 * we parsed it correctly. Weird syntax? Broken SQL? Target the
+	 * original tables.
+	 *
+	 * Should probably be substituted by a lexer instead. Which
+	 * will be pretty slow, unless we cache the parsed trees?
+	 *
 	 * @param string $string The SQL query.
 	 *
 	 * @return array Not sure yet
 	 */
 	public static function parse( string $query ) : array {
 		$result = [
-			'query'     => '',
-			'operation' => '',
-			'table'     => '',
-			'alias'     => '',
+			'query'      => '',
+			'operation'  => '',
+			'table'      => '',
+			'alias'      => '',
+			'joins'      => [],
+			'subqueries' => [],
+			'parsed'     => false,
 		];
 
 		/** First and foremost gobble up the operation */
-		if ( ! preg_match( "#^\s*(select|update|delete)\s+#i", $query, $matches ) ) {
+		if ( ! preg_match( "#^\s*(SELECT|UPDATE|DELETE)\s+#i", $query, $matches ) ) {
 			return $result;
 		}
 
@@ -68,40 +101,75 @@ class Query {
 		$result['query'] .= $matches[0];
 		$query = substr( $query, strlen( $matches[0] ) );
 
-		$table_definition = '\w[\d\w]*';
+		/** Generic patterns. */
+		$table_definition  = '\w[\d\w]*';
+		$column_definition = "(?:$table_definition\.)?$table_definition";
+		$operator          = '(?:\!?=|(?:NOT\s+)?LIKE|IS\s+(?:NOT\s+))';
 
 		/** Main table */
 		switch ( $result['operation'] ):
 			case 'SELECT':
-				if ( ! preg_match( $pattern = "#^\S*\s+FROM\s+`?($table_definition)`?\s*#i", $query, $matches ) ) {
+				if ( ! preg_match( $pattern = "#^(\S*\s+FROM\s+`?)($table_definition)(`?\s*)#i", $query, $matches ) ) {
 					return $result;
 				}
 				break;
 			case 'UPDATE':
-				if ( ! preg_match( $pattern = "#^`?($table_definition)`?\s*#i", $query, $matches ) ) {
+				if ( ! preg_match( $pattern = "#^(`?)($table_definition)(`?\s*)#i", $query, $matches ) ) {
 					return $result;
 				}
 				break;
 			case 'DELETE':
-				if ( ! preg_match( $pattern = "#^FROM\s+`?($table_definition)`?s*#i", $query, $matches ) ) {
+				if ( ! preg_match( $pattern = "#^(FROM\s+`?)($table_definition)(`?s*)#i", $query, $matches ) ) {
 					return $result;
 				}
 				break;
+			default:
+				return $result;
 		endswitch;
 
-		$result['table'] = $matches[1];
+		$result['table'] = $matches[2];
 
 		/** Wrap the table in a marker for replacement later on. */
-		$result['query'] .= preg_replace( $pattern, '[[$table:\1]]', $matches[0] );
+		$result['query'] .= preg_replace( $pattern, '\1[[$table:\2]]\3', $matches[0] );
 		$query = substr( $query, strlen( $matches[0] ) );
 
-		/** Alias? */
 		if ( 'SELECT' == $result['operation'] ) {
-			if ( preg_match( "#^(?:AS\s+)?`?(?!WHERE\s|ORDER\s|LIMIT\s)($table_definition)`?#", $query, $matches ) ) {
+			/** Alias? */
+			if ( preg_match( "#^(?:AS\s+)?`?(?!JOIN|LEFT|INNER|OUTER|WHERE|ORDER|LIMIT)($table_definition)`?s*#i", $query, $matches ) ) {
 				$result['alias'] = $matches[1];
+				$result['query'] .= $matches[0];
+				$query = substr( $query, strlen( $matches[0] ) );
+			}
+
+			/** Joins? */
+			while ( $query ) {
+				if ( ! preg_match( $pattern = "#^((?:LEFT|INNER|OUTER|)JOIN `?)($table_definition)(`?(?:\s+AS)?\s+`?)($table_definition)(`?\s*)#i", $query, $matches ) ) {
+					break;
+				}
+
+				$result['joins'] []= [
+					'table' => $matches[2],
+					'alias' => $matches[4],
+				];
+
+				$result['query'] .= preg_replace( $pattern, '\1[[$table:\2]]\3\4\5', $matches[0] );
+				$query = substr( $query, strlen( $matches[0] ) );
+
+				/** Goobble up the rest... */
+				if ( ! preg_match( "#^ON\s+(:?$column_definition)\s+$operator\s+(?:$column_definition)\s*#i", $query, $matches ) ) {
+					break;
+				}
+
+				$result['query'] .= $matches[0];
 				$query = substr( $query, strlen( $matches[0] ) );
 			}
 		}
+
+		/** Where */
+
+		$query = rtrim( $query, ';' );
+
+		$result['parsed'] = strlen( $query ) == 0;
 
 		return $result;
 	}
--- tests/test_query.php
+++ tests/test_query.php
@@ -2,33 +2,59 @@
 namespace metamirror;
 
 class TestQuery extends \WP_UnitTestCase {
-	public function test_parse_basic() {
+	public function test_parse_from() {
 		$result = Query::parse( 'SELECT * FROM `table1`' );
 		$this->assertEquals( 'SELECT', $result['operation'] );
 		$this->assertEquals( 'table1', $result['table'] );
+		$this->assertTrue( $result['parsed'] );
 
 		$result = Query::parse( '  update   od' );
 		$this->assertEquals( 'UPDATE', $result['operation'] );
 		$this->assertEquals( 'od', $result['table'] );
+		$this->assertTrue( $result['parsed'] );
 
 		$result = Query::parse( "\n\t delete \n from \n table2" );
 		$this->assertEquals( 'DELETE', $result['operation'] );
 		$this->assertEquals( 'table2', $result['table'] );
+		$this->assertTrue( $result['parsed'] );
 
 		$result = Query::parse( 'SELECT * FROM `table1` AS t1' );
 		$this->assertEquals( 'table1', $result['table'] );
 		$this->assertEquals( 't1', $result['alias'] );
+		$this->assertTrue( $result['parsed'] );
 
 		$result = Query::parse( 'SELECT * FROM `table1` AS `t1`' );
 		$this->assertEquals( 'table1', $result['table'] );
 		$this->assertEquals( 't1', $result['alias'] );
+		$this->assertTrue( $result['parsed'] );
 
 		$result = Query::parse( 'SELECT * FROM `table1` AS `t1` WHERE' );
 		$this->assertEquals( 'table1', $result['table'] );
 		$this->assertEquals( 't1', $result['alias'] );
+		$this->assertFalse( $result['parsed'] );
 
 		$result = Query::parse( 'SELECT * FROM `table1` AS t1 ORDER' );
 		$this->assertEquals( 'table1', $result['table'] );
 		$this->assertEquals( 't1', $result['alias'] );
+		$this->assertFalse( $result['parsed'] );
+
+		$result = Query::parse( 'SELECT * FROM `table1` WHERE' );
+		$this->assertEquals( 'table1', $result['table'] );
+		$this->assertEmpty( '', $result['alias'] );
+		$this->assertFalse( $result['parsed'] );
+
+		$result = Query::parse( 'SELECT * FROM `table1` JOIN table4 t ON post_id = t.post_id' );
+		$this->assertEquals( 'table1', $result['table'] );
+		$this->assertEmpty( '', $result['alias'] );
+		$this->assertTrue( $result['parsed'] );
+	}
+
+	public function test_parse_join() {
+		$result = Query::parse( 'SELECT * FROM `table1` JOIN table4 t ON post_id = t.post_id;' );
+		$this->assertEquals( 'table1', $result['table'] );
+		$joins = [ [ 'table' => 'table4', 'alias' => 't' ] ];
+		$this->assertEqualSets( $joins, $result['joins'] );
+		$this->assertTrue( $result['parsed'] );
+		$this->assertEquals( 'SELECT * FROM `[[$table:table1]]` JOIN [[$table:table4]] t ON post_id = t.post_id', $result['query'] );
 	}
 }
